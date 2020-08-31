e9710ce64be63a724b7798cd05e7b54eec01d676
Katie Hampson <katiehampson@gmail.com>
2020-08-31 11:50:00 +0100

    Added vial output
    Vaccine_vials.csv was derived from analysis (lines 217-233 fromPEP_IDvIM_v3.R)
    Used to inform assumptions on throughput for Gavi study (Lancet ID 2018)

--- rabies_PrEP_v3.R
+++ rabies_PrEP_v3.R
@@ -10,21 +10,21 @@ immunity_duration <- 20
 
 for(i in 1:10000){
 
-cost_PrEP_birth <- runif(1,5,20)
-cost_PEP_2 <- cost_PrEP_birth
+cost_PrEP_birth <- runif(1,5,20) # Cost of PrEP per dose - between $5-20
+cost_PEP_2 <- cost_PrEP_birth # Cost of PEP per dose - same as PrEP
 
-cost_PEP_4 <- cost_PrEP_birth + cost_PEP_2 + runif(1,0,120)
+cost_PEP_4 <- cost_PrEP_birth + cost_PEP_2 + runif(1,0,120) # Cost of PEP = Vaccine cost x 2 plus RIG between $0-120
 
-cost_ratio[i] <- cost_PEP_4/(cost_PrEP_birth + cost_PEP_2)
+cost_ratio[i] <- cost_PEP_4/(cost_PrEP_birth + cost_PEP_2) # RATIO = cost of PEP in naive / cost of PEP in boosted people
 
-incidence[i] <- runif(1,10,500)
+incidence[i] <- runif(1,10,500) # choose an incidence at random
 
-v_cost_PrEP_birth <- cost_PrEP_birth*birth_cohort
-v_cost_PEP_2 <- incidence[i]*immunity_duration*cost_PEP_2
+v_cost_PrEP_birth <- cost_PrEP_birth*birth_cohort # Costs of PrEP programme (100% coverage)
+v_cost_PEP_2 <- incidence[i]*immunity_duration*cost_PEP_2 # Costs of boosted PEP given bite incidence
 
-v_cost_PEP_4 <- incidence[i]*immunity_duration*cost_PEP_4
+v_cost_PEP_4 <- incidence[i]*immunity_duration*cost_PEP_4 # Costs of PEP in niave population
 
-v_cost_ratio[i] <- (v_cost_PrEP_birth + v_cost_PEP_2)/v_cost_PEP_4
+v_cost_ratio[i] <- (v_cost_PrEP_birth + v_cost_PEP_2)/v_cost_PEP_4 # Cost ratio
 
 }
 
@@ -36,7 +36,9 @@ myPalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")))
 my.labels <- c(ceiling(1.5*min(alldata$final_cost_ratio)),50,100,200)
 sc <- scale_colour_gradientn(colours = myPalette(100), limits=c(min(v_cost_ratio), max(v_cost_ratio)), breaks=my.labels, labels=my.labels)
 
-main <- ggplot(alldata) + geom_point(data=alldata,aes(x = incidence, y = cost_ratio, color = final_cost_ratio), size=3) + sc + ylab("ratio of cost") + theme_bw()
+main <- ggplot(alldata) + 
+  geom_point(data=alldata, aes(x = incidence, y = cost_ratio, color = final_cost_ratio), size=3) + 
+  sc + ylab("ratio of cost") + theme_bw()
    # geom_point(aes(x = 24, y = 1), size=3, pch=3, col="black")  + annotate("text", x = 24, y = 1.5, label = "Tanzania")
 
 pdf("figs/PrEP_sim.pdf", onefile = TRUE, height = 4, width = 7)
